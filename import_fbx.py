b45d6d37516211c48f3d00a1617b36683c165bfd
isjang08 <47512795+loveyourdaddy@users.noreply.github.com>
2024-05-23 13:59:10 +0900

    clean up

--- import_fbx.py
+++ import_fbx.py
@@ -20,32 +20,34 @@ def get_joint_hierarchy(root_joint):
     _get_hierarchy(root_joint)
     return hierarchy
 
-src_to_tgt_map = {}
-src_to_tgt_map["Hips"] = "Bip001FBXASC032Pelvis"
-src_to_tgt_map["Spine"] = "Bip001FBXASC032Spine"
-src_to_tgt_map["Spine1"] ="Bip001FBXASC032Spine1"
-src_to_tgt_map["Spine2"] ="Bip001FBXASC032Spine2"
-src_to_tgt_map["Neck"] = "Bip001FBXASC032Neck"
-src_to_tgt_map["Neck1"] = "Bip001FBXASC032Head"
-src_to_tgt_map["head"] = "Bip001FBXASC032HeadNub"
+# src_to_tgt_map
+if True:
+    src_to_tgt_map = {}
+    src_to_tgt_map["Hips"] = "Bip001FBXASC032Pelvis"
+    src_to_tgt_map["Spine"] = "Bip001FBXASC032Spine"
+    src_to_tgt_map["Spine1"] ="Bip001FBXASC032Spine1"
+    src_to_tgt_map["Spine2"] ="Bip001FBXASC032Spine2"
+    src_to_tgt_map["Neck"] = "Bip001FBXASC032Neck"
+    src_to_tgt_map["Neck1"] = "Bip001FBXASC032Head"
+    src_to_tgt_map["head"] = "Bip001FBXASC032HeadNub"
 
-src_to_tgt_map["LeftShoulder"] = "Bip001FBXASC032LFBXASC032Clavicle"
-src_to_tgt_map["LeftArm"]      = "Bip001FBXASC032LFBXASC032UpperArm"
-src_to_tgt_map["LeftForeArm"]  = "Bip001FBXASC032LFBXASC032Forearm"
-src_to_tgt_map["LeftHand"]     = "Bip001FBXASC032LFBXASC032Hand"
-src_to_tgt_map["RightShoulder"]= "Bip001FBXASC032RFBXASC032Clavicle"
-src_to_tgt_map["RightArm"]     = "Bip001FBXASC032RFBXASC032UpperArm"
-src_to_tgt_map["RightForeArm"] = "Bip001FBXASC032RFBXASC032Forearm"
-src_to_tgt_map["RightHand"]    = "Bip001FBXASC032RFBXASC032Hand"
+    src_to_tgt_map["LeftShoulder"] = "Bip001FBXASC032LFBXASC032Clavicle"
+    src_to_tgt_map["LeftArm"]      = "Bip001FBXASC032LFBXASC032UpperArm"
+    src_to_tgt_map["LeftForeArm"]  = "Bip001FBXASC032LFBXASC032Forearm"
+    src_to_tgt_map["LeftHand"]     = "Bip001FBXASC032LFBXASC032Hand"
+    src_to_tgt_map["RightShoulder"]= "Bip001FBXASC032RFBXASC032Clavicle"
+    src_to_tgt_map["RightArm"]     = "Bip001FBXASC032RFBXASC032UpperArm"
+    src_to_tgt_map["RightForeArm"] = "Bip001FBXASC032RFBXASC032Forearm"
+    src_to_tgt_map["RightHand"]    = "Bip001FBXASC032RFBXASC032Hand"
 
-src_to_tgt_map["LeftUpLeg"]    = "Bip001FBXASC032LFBXASC032Thigh"
-src_to_tgt_map["LeftLeg"]      = "Bip001FBXASC032LFBXASC032Calf"
-src_to_tgt_map["LeftFoot"]     = "Bip001FBXASC032LFBXASC032Foot"
-src_to_tgt_map["LeftToeBase"]  = "Bip001FBXASC032LFBXASC032Toe0"
-src_to_tgt_map["RightUpLeg"]   = "Bip001FBXASC032RFBXASC032Thigh"
-src_to_tgt_map["RightLeg"]     = "Bip001FBXASC032RFBXASC032Calf"
-src_to_tgt_map["RightFoot"]    = "Bip001FBXASC032RFBXASC032Foot"
-src_to_tgt_map["RightToeBase"] = "Bip001FBXASC032RFBXASC032Toe0"
+    src_to_tgt_map["LeftUpLeg"]    = "Bip001FBXASC032LFBXASC032Thigh"
+    src_to_tgt_map["LeftLeg"]      = "Bip001FBXASC032LFBXASC032Calf"
+    src_to_tgt_map["LeftFoot"]     = "Bip001FBXASC032LFBXASC032Foot"
+    src_to_tgt_map["LeftToeBase"]  = "Bip001FBXASC032LFBXASC032Toe0"
+    src_to_tgt_map["RightUpLeg"]   = "Bip001FBXASC032RFBXASC032Thigh"
+    src_to_tgt_map["RightLeg"]     = "Bip001FBXASC032RFBXASC032Calf"
+    src_to_tgt_map["RightFoot"]    = "Bip001FBXASC032RFBXASC032Foot"
+    src_to_tgt_map["RightToeBase"] = "Bip001FBXASC032RFBXASC032Toe0"
 
 # strDir = "D:/2024_KAI_Retargeting/Adori/animation/0055_Freestyle002_03_RT0214.fbx"
 # mel.eval('FBXImport -f"{}"'.format(strDir))
@@ -61,7 +63,6 @@ perjoint_data = {'rotateX': [], 'rotateY': [], 'rotateZ': [],
                  'translateX': [], 'translateY': [], 'translateZ': []}
 max_val = 0
 for object_name in joint_hierarchy:
-    
     if object_name not in src_to_tgt_map.keys():
         print(object_name, "not found")
         continue
@@ -84,7 +85,6 @@ for object_name in joint_hierarchy:
     datas.append(perjoint_data)
 
 """ update """
-duration = 0.25
 if False:
     len_frame = int(max_val / duration) + 1
     print("len_frame:", len_frame) # 2569.0 # 642.25
@@ -126,20 +126,19 @@ if False:
 # # targetDir = "D:/2024_KAI_Retargeting/bear.fbx"
 # # # mel.eval('FBXImport -f"{}"'.format(targetDir))
 for i, (src_joint, tgt_joint) in enumerate(src_to_tgt_map.items()):
-    if i!=0:
-        continue
+    # if i!=0:
+    #     continue
     perjoint_data = datas[i]
     # print("src:{}, tgt:{}, ".format(src_joint, tgt_joint))
     if src_joint=="Hips":
         array = ['rotateX', 'rotateY', 'rotateZ', "translateX", "translateY", "translateZ",]
     else:
         array = ['rotateX', 'rotateY', 'rotateZ']
-    # print("tgt:{}, {}".format(tgt_joint, array))
 
     for attr in array: 
         value = perjoint_data[attr]
         # print("value:", value)
         for times, rot in value:
             if rot is not None:
-                print("attr: {}, times: {}, rot: {}".format(attr, times, rot))
+                # print("attr: {}, times: {}, rot: {}".format(attr, times, rot))
                 cmds.setKeyframe(tgt_joint, attribute=attr, t=times, v=rot)
