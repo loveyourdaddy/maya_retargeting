47b00b8249b4a2657bb69d66312ddb769421d2da
isjang08 <47512795+loveyourdaddy@users.noreply.github.com>
2024-05-27 14:45:32 +0900

    chage args from dir to path

--- import_export_fbx.py
+++ import_export_fbx.py
@@ -19,9 +19,9 @@ if not cmds.pluginInfo('fbxmaya', query=True, loaded=True):
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Import an FBX file into Maya')
-    parser.add_argument('--SrcMotionDir', type=str, default="D:/2024_KAI_Retargeting/Adori/animation/0055_Freestyle002_03_RT0214.fbx")
-    parser.add_argument('--TgtCharDir', type=str, default="D:/2024_KAI_Retargeting/Adori/SKM_ADORI_0229.fbx")
-    parser.add_argument('--TgtMotionDir', type=str, default="D:/2024_KAI_Retargeting/Adori/exported.fbx")
+    parser.add_argument('--src_motion_path', type=str, default="D:/2024_KAI_Retargeting/Adori/animation/0055_Freestyle002_03_RT0214.fbx")
+    parser.add_argument('--tgt_char_path', type=str, default="D:/2024_KAI_Retargeting/Adori/SKM_ADORI_0229.fbx")
+    parser.add_argument('--tgt_motion_path', type=str, default="D:/2024_KAI_Retargeting/Adori/exported.fbx")
     return parser
 
 def get_args():
