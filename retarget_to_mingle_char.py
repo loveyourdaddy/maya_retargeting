755ba8574bc71aa7a1dde29615457e0855c02842
isjang08 <47512795+loveyourdaddy@users.noreply.github.com>
2024-06-04 21:00:40 +0900

    euler angle, first frame right, but angle setting is wrong

--- retarget_to_mingle_char.py
+++ retarget_to_mingle_char.py
@@ -88,38 +88,6 @@ def R_to_E(R):
     # return alpha, beta, gamma
     return np.array([alpha, beta, gamma])
 
-def E_to_R(E, order="zyx", radians=True): # xyz
-    """
-    Args:
-        E: (..., 3)
-    """
-    if E.shape[-1] != 3:
-        raise ValueError(f"Invalid Euler angles shape {E.shape}")
-    if len(order) != 3:
-        raise ValueError(f"Order must have 3 characters, but got {order}")
-
-    if not radians:
-        E = np.deg2rad(E)
-
-    def _euler_axis_to_R(angle, axis):
-        one  = np.ones_like(angle, dtype=np.float32)
-        zero = np.zeros_like(angle, dtype=np.float32)
-        cos  = np.cos(angle, dtype=np.float32)
-        sin  = np.sin(angle, dtype=np.float32)
-
-        if axis == "x":
-            R_flat = (one, zero, zero, zero, cos, -sin, zero, sin, cos)
-        elif axis == "y":
-            R_flat = (cos, zero, sin, zero, one, zero, -sin, zero, cos)
-        elif axis == "z":
-            R_flat = (cos, -sin, zero, sin, cos, zero, zero, zero, one)
-        else:
-            raise ValueError(f"Invalid axis: {axis}")
-        return np.stack(R_flat, axis=-1).reshape(angle.shape + (3, 3))
-
-    R = [_euler_axis_to_R(E[..., i], order[i]) for i in range(3)]
-    return np.matmul(np.matmul(R[0], R[1]), R[2])
-
 """ src_to_tgt_map """
 if True:
     src_to_tgt_map = {}
@@ -257,7 +225,7 @@ if True:
         for object_name in joint_hierarchy:
             if object_name not in src_to_tgt_map.keys():
                 continue
-            src_rot = get_rot_matrix(object_name) # get_rotation
+            src_rot = get_rotation(object_name) # get_rot_matrix
             src_frame_rot.append(src_rot)
         src_rots.append(src_frame_rot)
 
@@ -328,21 +296,16 @@ if True:
                 src_rot = np.array(src_rots[tid][i])
                 
                 # src
-                # print("inv_src_trf:", inv_src_trf)
-                # print("src_rot:", src_rot)
-                src_origin_rot = inv_src_trf @ src_rot # src_Tpose_rots
-                origin_angle = R_to_E(src_origin_rot)
-                # print("origin_angle:", origin_angle)
+                print("inv_src_trf:", inv_src_trf)
+                print("src_rot:", src_rot)
+                origin_angle = inv_src_trf @ src_rot # src_Tpose_rots
+                print("origin_angle:", origin_angle)
                 origin_angle[1] += -90
                 origin_angle[2] += -90
-                
                 # tgt
-                tgt_origin_angle = origin_angle
-                tgt_origin_angle[0] += 180
-                tgt_origin_angle[1] += 90
-                tgt_origin_mat = E_to_R(tgt_origin_angle)
-                tgt_origin_mat = tgt_trf @ tgt_origin_mat
-                tgt_rot = R_to_E(tgt_origin_mat)
+                origin_angle[0] += 180
+                origin_angle[1] += 90
+                tgt_rot = tgt_trf @ origin_angle
                 
                 for eid, attr in enumerate(array):
                     cmds.setKeyframe(tgt_joint, attribute=attr, t=time, v=tgt_rot[eid])
