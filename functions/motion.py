39a75db07cf3aa67721458d433e19d1a4f1aefcb
isjang08 <isjang08@kaist.ac.kr>
2024-07-16 18:07:13 +0900

    debugging

--- functions/motion.py
+++ functions/motion.py
@@ -1,4 +1,3 @@
-
 from functions.joints import *
 from functions.keyframe import *
 from functions.rotations import *
@@ -9,7 +8,8 @@ def retarget_translation(src_hip, tgt_hip, tgt_locator, tgt_locator_rot, tgt_loc
     trans_attr = {'translateX': [], 'translateY': [], 'translateZ': []}
     trans_data = get_array_from_keyframe_data(trans_data, trans_attr)
     len_frame = len(trans_data)
-    if len_frame != 0:
+    print(len_frame)
+    if len_frame!=0:
         if len(tgt_locator)!=0:
             tgt_locator_rot_mat = E_to_R(-1 * np.array(tgt_locator_rot))
         else:
@@ -26,30 +26,28 @@ def retarget_translation(src_hip, tgt_hip, tgt_locator, tgt_locator_rot, tgt_loc
     return trans_data
 
 def retarget_rotation(src_joints, tgt_joints, Tpose_trfs, parent_indices, tgt_locator_rot,\
-                      len_frame,):
+                      len_frame):
     # rotation
     tgt_world_mats = np.full((len_frame, len(tgt_joints), 3, 3), None, dtype=np.float32)
     for j, (src_joint, tgt_joint) in enumerate(zip(src_joints, tgt_joints)):
-        # print("{} {} {}".format(j, src_joint, tgt_joint))
-
-        """ src """
-        # keyframe_data [attr, frames, (frame, value)]: (trans, world rot)
+        # keyframe_data 
+        # [attr, frames, (frame, value)]: (trans, world rot)
         _, rot_data = get_keyframe_data(src_joint)
 
-        """ tgt target angle from src """
+        # tgt target angle from src
         # src: world rotation for tgt
         rot_attr = {'rotateX': [], 'rotateY': [], 'rotateZ': []}
         rot_data = get_array_from_keyframe_data(rot_data, rot_attr)
 
-        """ update data """
+        # update data
         desired_rot_data = np.full((len_frame+1, 3), None, dtype=np.float32)
         for i in range(len_frame):
             """ src """
-            # src world angle
+            # world angle
             src_local_angle = rot_data[i]
             src_local_mat = E_to_R(src_local_angle)
             
-            # src parent angle 
+            # parent angle 
             if j==0:
                 # locator
                 src_parent_rot_mat = E_to_R(np.array([0,0,0])) # src_locator_rot TODO: src가 다를때 확인 
@@ -59,23 +57,32 @@ def retarget_rotation(src_joints, tgt_joints, Tpose_trfs, parent_indices, tgt_lo
                 src_parent_rot_mat = np.transpose(np.array(cmds.xform(src_parent_joint, q=True, ws=True, matrix=True)).reshape(4,4))[:3,:3]
             src_world_mat = src_parent_rot_mat @ src_local_mat
 
-            # target world angle 
+            """ tgt """
+            # world angle 
             tgt_world_mat = src_world_mat @ Tpose_trfs[j]
             tgt_world_mats[i, j] = tgt_world_mat
 
             # update by frame
             if j==0:
                 # locator
-                tgt_parent_rot_mat = E_to_R(np.array(tgt_locator_rot))
+                tgt_parent_rotmat = E_to_R(np.array(tgt_locator_rot))
+                tgt_parent_rotmat_old = np.identity(1)
+                parent_j = -1
             else:
                 # tgt parent world rot
                 parent_j = parent_indices[j]
-                tgt_parent_rot_mat = tgt_world_mats[i, parent_j]
-                # if i==0:
-                #     print("tgt_parent_rot_mat {}: \n{}".format(parent_j, tgt_parent_rot_mat))
-            tgt_local_mat = np.linalg.inv(tgt_parent_rot_mat) @ tgt_world_mat
+                tgt_parent_rotmat = tgt_world_mats[i, parent_j]
+
+                tgt_parent_joint = get_parent_joint(tgt_joint)
+                tgt_parent_rotmat_old = np.transpose(np.array(cmds.xform(tgt_parent_joint, q=True, ws=True, matrix=True)).reshape(4,4))[:3,:3] 
+
+            tgt_local_mat = np.linalg.inv(tgt_parent_rotmat) @ tgt_world_mat
             tgt_local_angle = R_to_E(tgt_local_mat)
             desired_rot_data[i] = tgt_local_angle
+
+            if i==100:
+                # print("{} {} {}".format(j, parent_j, src_joint))
+                print("{} {} \n{}src \n{}tgt \n{}old".format(j, src_joint, src_parent_rot_mat, tgt_parent_rotmat, tgt_parent_rotmat_old))
 
         # update by joint
         set_keyframe(tgt_joint, desired_rot_data, rot_attr)
