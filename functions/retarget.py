[bug fix] fix rotation order from bvh

functions/retarget.py
@@ -454,7 +454,7 @@ def retarget_translation(src_hip, tgt_hip,
         delta_R = np.linalg.inv(Tpose_root_R_ext) @ root_R
         # delta_R = root_R @ np.linalg.inv(Tpose_root_R_ext)
 
-            diff_vec = subchain_local_diff_vec[i]
+        diff_vec = subchain_local_diff_vec[i]
         # update diff vector
         diff_vec = subchain_local_diff_vec[i]
         diff_vec_ext = np.repeat(diff_vec[None,:], repeats=len_frame, axis=0)[:,:,None]
@@ -548,9 +548,18 @@ def retarget_rotation(src_common_joints, src_Tpose_localrots, # src {}
                 final_matrix = np.array(final_matrix).reshape(4, 4)
                 euler_angle = R_to_E(final_matrix, order='xyz') # MAYA rotation order: XYZ
                 tgt_perjoint_local_angle[frame] = euler_angle
+            
+            # if j==0:
+            #     for f in range(len_frame):
+            #         print(f"{f}: {tgt_perjoint_local_angle[f]}")
+            #     import pdb; pdb.set_trace()
 
             # Refine angle as continue value
             tgt_perjoint_local_angle = unwrap_rotation(tgt_perjoint_local_angle)
+
+            # if j==0:
+            #     print(tgt_perjoint_local_angle)
+            #     import pdb; pdb.set_trace()
 
             # update 
             set_keyframe(tgt_joint, tgt_perjoint_local_angle, rot_attr)
@@ -560,7 +569,6 @@ def retarget_rotation(src_common_joints, src_Tpose_localrots, # src {}
         # update
         tgt_perjoint_local_angle = set_keyframe_for_joint(tgt_joint, target_tpose_matrix, Tpose_trfs[j], j)
         tgt_local_angles[:, j] = tgt_perjoint_local_angle
-
 
         # subchain
         for chain_id, subjoint_jid in enumerate(subchain_indices):